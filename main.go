f4f6acfacbea78d10d86a76757c8e28e4b1d7b94
haris <haris.aulia404@gmail.com>
2023-03-07 23:26:01 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+//Harisatul Aulia - GOLANG 2
+import "fmt"
+
+func main() {
+	var i = 21
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%T\n", i)
+	fmt.Println("%")
+	var j = true
+	fmt.Println(j)
+	//Harisatul Aulia - GOLANG 2
+	fmt.Printf("%b\n", i)
+	char := 'Я'
+	fmt.Printf("%c\n", char)
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%o\n", i)
+	f := 15
+	//Harisatul Aulia - GOLANG 2
+	fmt.Printf("%x\n", f)
+	fmt.Printf("%X\n", f)
+	fmt.Printf("%U\n", char)
+
+	var k float64 = 123.456
+	fmt.Printf("%.6f\n", k)
+	fmt.Printf("%e\n", k)
+	//Harisatul Aulia - GOLANG 2
+
+}
